e740d1498de949cdffea601e9cff5245b74e0cd0
victoriaschwint <victoria.schwint@aizon.com>
2023-10-08 14:09:36 +0200

    Make the test fail

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,5 @@ import App from './App';
 test('renders learn react link', () => {
  render(<App />);
  const linkElement = screen.getByText(/Get Started/i);
- expect(linkElement).toBeInTheDocument();
+ expect(linkElement).not.toBeInTheDocument();
 });
